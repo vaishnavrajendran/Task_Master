a5ca7b9232fd7e33025370d5c5dbc5a5c9d88ab0
Vaishnav R <vaishnavrajendran95@gmail.com>
2023-03-24 12:35:15 +0530

    baseurl

--- client/src/config/axios.js
+++ client/src/config/axios.js
@@ -1,6 +1,5 @@
 import axios from 'axios';
 
 export default axios.create({
-    // baseURL: "https://task-server-5neu.onrender.com"
-    baseURL: "http://localhost:3001"
+    baseURL: "https://task-server-5neu.onrender.com"
 })
